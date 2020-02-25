65a7651fba9f3eb72cea7175684b4b8baac04a88
unknown <hosea.yoarana@ui.ac.id>
2020-02-25 18:27:50 +0700

    modul1

---
+++ JFood.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class JFood here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JFood
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class JFood
+     */
+    public static void main(String[] args)
+    {
+        // initialise instance variables
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+}
